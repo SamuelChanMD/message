08185b2bb7c7c6082b7c26874291b4615f1a7d34
SamuelChanMD <samuelchanmd@gmail.com>
2021-04-11 20:09:55 -0400

    db vars

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const {Pool} = require('pg');
 const logger = require('./logger.js');
 
 const pool = new Pool({
-    'host': process.env.RDS_HOST || '127.0.0.1',
+    'host': process.env.RDS_HOSTNAME || '127.0.0.1',
     'port': 5432,
     'user': process.env.RDS_USERNAME || 'postgres',
     'password': process.env.RDS_PASSWORD || 'irwtj21!',
@@ -16,7 +16,9 @@ const pool = new Pool({
     'idleTimeoutMillis': 0,
 });
 
-logger.warn(`does rds_host exist? ${process.env.RDS_HOST}`);
+logger.warn(`does rds_hostname exist? ${process.env.RDS_HOSTNAME}`);
 logger.warn(`does db host exist? ${process.env.DB_HOST}`);
+logger.warn(`does rds_db_name exist? ${process.env.RDS_DB_NAME}`);
+logger.warn(`does db database exist? ${process.env.DB_DATABASE}`);
 
 module.exports = pool;
