0d2203bfe86832a9c5dff5ad11573809955ad3de
SamuelChanMD <samuelchanmd@gmail.com>
2021-04-11 20:00:58 -0400

    db vars

--- db.js
+++ db.js
@@ -5,11 +5,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    'host': '127.0.0.1',
+    'host': process.env.RDS_HOST || '127.0.0.1',
     'port': 5432,
-    'user': 'postgres',
-    'password': 'irwtj21!',
-    'database': 'message',
+    'user': process.env.RDS_USERNAME || 'postgres',
+    'password': process.env.RDS_PASSWORD || 'irwtj21!',
+    'database': process.env.RDS_DB_NAME || 'message',
     'max': 3,
     'connectionTimeoutMillis': 10000, // 10 seconds
     'idleTimeoutMillis': 0,
